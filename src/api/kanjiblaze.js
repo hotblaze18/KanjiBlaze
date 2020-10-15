8b403dcf32af0ffb052a9bd068be53e3c1e3d789
pranav <arora.pranav.2000@gmail.com>
2020-10-15 16:48:43 +0530

    Rectified baseURL

--- src/api/kanjiblaze.js
+++ src/api/kanjiblaze.js
@@ -1,4 +1,4 @@
-const baseURL = "http://kanjiblaze.herokuapp.com/login";
+const baseURL = "https://kanjiblaze-api.herokuapp.com";
 const settings = {
   withCredentials: true,
   headers: { "Access-Control-Allow-Credentials": true },
