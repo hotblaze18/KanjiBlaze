8fe98d93794f9584c921dc0c3c838b7e2907ec1e
pranav <arora.pranav.2000@gmail.com>
2020-10-17 15:39:26 +0530

    UPDATED SIGNUP UI

--- src/components/SignupUser.js
+++ src/components/SignupUser.js
@@ -13,27 +13,32 @@ const SignupUser = (props) => {
   };
 
   return (
-    <div>
-      <Header />
-      <form onSubmit={submitForm}>
-        <div>
-          <span>Name:</span>
-          <input type="text" name="name" placeholder="ENTER NAME"></input>
-        </div>
-        <div>
-          <span>Email:</span>
-          <input type="email" name="email" placeholder="ENTER EMAIL"></input>
-        </div>
-        <div>
-          <span>Passowrd:</span>
-          <input
-            type="password"
-            name="password"
-            placeholder="ENTER PASSWORD"
-          ></input>
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+    <div id="signupform">
+    <form onSubmit={(e) => submitForm(e)} class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
+    <div class="mb-4">
+      <label class="block text-grey-darker text-sm font-bold mb-2" for="username">
+        Name
+      </label>
+      <input name="name" class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="name" type="text" placeholder="Name" />
+    </div>
+    <div class="mb-4">
+      <label class="block text-grey-darker text-sm font-bold mb-2" for="username">
+        Email
+      </label>
+      <input name="email" class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="email" type="text" placeholder="Email" />
+    </div>
+    <div class="mb-6">
+      <label class="block text-grey-darker text-sm font-bold mb-2" for="password">
+        Password
+      </label>
+      <input name="password" class="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3" id="password" type="password" placeholder="******************" />
+    </div>
+    <div class="flex items-center justify-between">
+      <button class=" bg-primary hover:bg-blue-dark text-white font-bold py-2 px-4 rounded" type="submit">
+        Sign Up
+      </button>
+    </div>
+  </form>
     </div>
   );
 };
